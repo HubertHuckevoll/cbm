0c221c954c023626dbc7cb6fdb556d3994a03104
HubertHuckevoll <konstantin.meyer@gmail.com>
2023-06-11 08:45:07 +0200

    remove duplicate code

--- lb/md/cbmArticleFactoryM.php
+++ lb/md/cbmArticleFactoryM.php
@@ -27,51 +27,6 @@ class cbmArticleFactoryM
     return $result;
   }
 
-  public function search(string $term): array
-  {
-    $data = [];
-    $result = [];
-    $i = 0;
-
-    $term = mb_strtolower($term);
-
-    foreach($this->indexData as $articleEntry)
-    {
-      if (!isset($articleEntry)) break;
-      $data = [];
-      $article = new cbmArticleM($articleEntry['store'], $articleEntry['articleBox'], $articleEntry['articleName']);
-      $data = $article->get();
-
-      foreach($data as $key => $val)
-      {
-        if ($key !== 'images')
-        {
-          $val = mb_strtolower($term);
-          $val = strip_tags($val);
-          if (($pos = mb_stripos($val, $term)) !== false)
-          {
-            $hit = $this->extractHit($val, $term, $pos);
-            $hit = str_ireplace($term, '<strong>'.$term.'</strong>', $hit);
-            array_push($result, [
-              'article' => $article,
-              'hit' => $hit
-            ]);
-          }
-        }
-      }
-    }
-
-    return $result;
-  }
-
-  protected function extractHit($text, $searchStr, $pos)
-  {
-    $start = ($pos - 75 > 0) ? $pos - 75 : 0;
-    $hit = substr($text, $start, (strlen($searchStr) + 150));
-    $hit = '...'.$hit.'...';
-    return $hit;
-  }
-
   /**
    * Summary of get
    * @param int $startIdx
